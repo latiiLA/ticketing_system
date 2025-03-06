b88e1047d762d78a42e5cc65b19c424a148ad83d
Lata <amelatbay@gmail.com>
2025-03-06 11:07:38 +0300

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import Login from "./Pages/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
-import Dashboard from "./Pages/Dashboard";
+import Dashboard from "./Pages/AdminDashboard";
 import Tickets from "./Pages/Tickets";
+import UserDashboard from "./Pages/UserDashboard";
+import MyTickets from "./Pages/MyTickets";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/tickets" element={<Tickets />} />
+      <Route element={<Dashboard />}>
+        <Route path="/admindashboard" element={<div>Dashboard Content</div>} />
+        <Route path="/tickets" element={<Tickets />} />
+      </Route>
+      <Route element={<UserDashboard />}>
+        <Route path="/dashboard" element={<div>Dashboard Content</div>} />
+        <Route path="/mytickets" element={<MyTickets />} />
+      </Route>
       {/* <Route path="/changepassword" /> */}
     </Routes>
   );
