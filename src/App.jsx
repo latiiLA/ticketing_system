cf160fb45ec1943d261c39aa8f42b8225182b84c
Lata <amelatbay@gmail.com>
2025-03-06 21:54:30 +0300

    Toaster and new routes added

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,32 @@ import Dashboard from "./Pages/AdminDashboard";
 import Tickets from "./Pages/Tickets";
 import UserDashboard from "./Pages/UserDashboard";
 import MyTickets from "./Pages/MyTickets";
+import { Toaster } from "react-hot-toast";
+import { Box } from "@mui/material";
+import CreateTickets from "./Pages/CreateTickets";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route element={<Dashboard />}>
-        <Route path="/admindashboard" element={<div>Dashboard Content</div>} />
-        <Route path="/tickets" element={<Tickets />} />
-      </Route>
-      <Route element={<UserDashboard />}>
-        <Route path="/dashboard" element={<div>Dashboard Content</div>} />
-        <Route path="/mytickets" element={<MyTickets />} />
-      </Route>
-      {/* <Route path="/changepassword" /> */}
-    </Routes>
+    <Box>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<Dashboard />}>
+          <Route
+            path="/admindashboard"
+            element={<div>Dashboard Content</div>}
+          />
+          <Route path="/managetickets" element={<Tickets />} />
+        </Route>
+        <Route element={<UserDashboard />}>
+          <Route path="/dashboard" element={<div>Dashboard Content</div>} />
+          <Route path="/tickets" element={<CreateTickets />} />
+          <Route path="/mytickets" element={<MyTickets />} />
+        </Route>
+        {/* <Route path="/changepassword" /> */}
+      </Routes>
+      <Toaster />
+    </Box>
   );
 }
 
