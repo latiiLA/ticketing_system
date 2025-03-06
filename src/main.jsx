b03b8652c67553e384e0d9c925f9982b97a59b02
Lata <amelatbay@gmail.com>
2025-03-06 21:54:07 +0300

    clg production env removal

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
+if (import.meta.env.VITE_APP_NODE_ENV === "production") {
+  console.log = () => {};
+}
+
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
   <Router>
