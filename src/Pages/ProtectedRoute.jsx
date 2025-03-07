b6592035fe2cd893935a5303932819c2e79fb0a0
Lata <amelatbay@gmail.com>
2025-03-07 16:19:22 +0300

    protected route functionality

---
+++ src/Pages/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+export default ProtectedRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 if (import.meta.env.VITE_APP_NODE_ENV === "production") {
   console.log = () => {};
@@ -10,8 +11,12 @@ if (import.meta.env.VITE_APP_NODE_ENV === "production") {
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
+
   <Router>
+    {/* <AuthContextProvider> */}
     <App />
+    {/* </AuthContextProvider> */}
   </Router>
+
   // </StrictMode>
 );
